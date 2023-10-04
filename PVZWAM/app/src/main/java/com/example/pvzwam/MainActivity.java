90e6e543cec07382b84318a5095310a8cf48e8b2
samanthaneri <samneri@iastate.edu>
2023-10-04 11:45:33 -0500

    update javadoc

--- PVZWAM/app/src/main/java/com/example/pvzwam/MainActivity.java
+++ PVZWAM/app/src/main/java/com/example/pvzwam/MainActivity.java
@@ -10,17 +10,22 @@ import android.view.View;
 import android.widget.TextView;
 import static java.lang.String.valueOf;
 
+/**
+ * Main activity for the game app. Brings the user to a home page
+ * that displays high score and last score, with button that triggers game play.
+ */
 public class MainActivity extends AppCompatActivity {
 
-
-//    Button play = findViewById(R.id.button_Play);
-
+    // Initializes textViews and score values
     private TextView highscore;
     private TextView lastscore;
     private int highScore;
-
     private int lastScore;
 
+    /**
+     * Initializes main activity page and values for high score and last score
+     * @param savedInstanceState Bundle containing save state data
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         highscore = findViewById(R.id.textView_hsNum);
         lastscore = findViewById(R.id.textView_lsNum);
 
+        //Shared preferences that obtain values for high and last score
         SharedPreferences pref = getSharedPreferences("scores", Context.MODE_PRIVATE);
         highScore = pref.getInt("high_score", 0);
         lastScore = pref.getInt("last_score", 0);
@@ -38,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * When user clicks the play button, the user is taken to the
+     * GameActivity page. If error occurs, runtime exception is thrown
+     * @param view play button is clicked
+     */
     public void onPlayClicked(View view) {
         try {
             Intent intent = new Intent(this, GameActivity.class);
